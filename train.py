862405d8ec7c600f4546109db23fdac5cfc4ace8
newwares <newwares123@gmail.com>
2023-09-19 19:03:14 -0400

    lgbm

--- train.py
+++ train.py
@@ -1,22 +1,22 @@
-
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import train_test_split, cross_val_score
-
 from preprocess import preprocess
+import lightgbm as lgb
 
 data = preprocess(pd.read_csv('train.csv'))
 ans = data['Survived']
 data = data.drop(['Survived'], axis=1)
-x_train, x_test, y_train, y_test = train_test_split(data, ans, test_size=0.2, random_state=69)
 print(data.head())
-model = GradientBoostingClassifier(n_estimators=700, learning_rate=0.1, max_depth=3, random_state=69)
-model = model.fit(x_train, y_train)
-test_scores = cross_val_score(model, x_test, y_test, cv=5)
-train_scores = cross_val_score(model, x_train, y_train, cv=5)
-print(train_scores)
-print(test_scores)
+data = lgb.Dataset(data, label=ans)
+params = {
+    'objective': 'binary', # binary classification
+    'num_leaves': 15, # num leaves should be less than 2^max_depth,
+    'min_data_in_leaf': 100, # generally 100s to 1000s
+    'metric': 'auc' # "best metric" for classification bc it's immune to unbalanced data
+}
+model = lgb.train(params, data, 10)
+print(min(lgb.cv(params, data, 10, nfold=5)['valid auc-mean'])) # maximize this value
+
 test = preprocess(pd.read_csv('test.csv'))
 predictions = model.predict(test)
-output = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': predictions}, dtype=int)
+output = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': predictions.round()}, dtype=int)
 output.to_csv('output.csv', index=False)
