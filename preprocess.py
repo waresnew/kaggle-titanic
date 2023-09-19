c787a6897cc8fc5e3ddf12b9ae4e74894615346d
newwares <newwares123@gmail.com>
2023-09-19 17:15:05 -0400

    77.272% acc

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,39 @@
 import pandas as pd
 import numpy as np
-import math
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
 
 
 def preprocess(data):
-    hot_embarked = pd.get_dummies(data['Embarked'], prefix='Embarked')
-    hot_sex = pd.get_dummies(data['Sex'], prefix='Sex')
-    data = data.drop(['Name', 'Cabin', 'Ticket', 'Embarked', 'Sex'], axis=1)
-    data = data.join(hot_embarked).join(hot_sex)
-    mean_age = math.floor(data['Age'].mean())+0.5 # estimates end with 0.5 in existing data so be consistent
-    data.fillna(mean_age, inplace=True)
+    data['Cabin'] = data['Cabin'].apply(lambda x: 0 if pd.isna(x) else 1)
+    data['Family'] = data['SibSp'] + data['Parch']+1
+    data['Alone'] = data['Family'].apply(lambda x: 1 if x == 1 else 0)
+    data['Title'] = data['Name'].apply(lambda x: x.split(',')[1].split('.')[0].strip())
+    # replace uncommon titles with "Other"
+    data['Title'] = data['Title'].replace(['Lady', 'the Countess', 'Capt', 'Col', 'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Other')
+    data['Title'] = data['Title'].replace('Mlle', 'Miss')
+    data['Title'] = data['Title'].replace('Ms', 'Miss')
+    data['Title'] = data['Title'].replace('Mme', 'Mrs')
+
+    num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='most_frequent'))
+    ])
+    cat_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('one_hot', OneHotEncoder(sparse_output=False)) # need dense array to output as dataframe
+    ])
+    data_pipeline = ColumnTransformer([
+        ('num', num_pipeline, ['Age', 'Fare']),
+        ('cat', cat_pipeline, ['Embarked', 'Sex', 'Title']),
+
+    ])
+    data_pipeline.set_output(transform='pandas')
+    extra = data_pipeline.fit_transform(data)
+    data = data.drop(['Name', 'Title', 'SibSp', 'Parch', 'Ticket', 'Age', 'Embarked', 'Sex', 'Fare'], axis=1)
+    data = pd.concat([data, extra], axis=1)
     data = data.astype(np.float32)
     return data
 
--- train.py
+++ train.py
@@ -11,6 +11,6 @@ data = data.drop(['Survived'], axis=1)
 print(data.head())
 model = get_model()
 print(model.summary())
-checkpoint = keras.callbacks.ModelCheckpoint('model.keras', save_best_only=True, save_weights_only=True, mode='max', monitor='accuracy')
-model.fit(data, ans, epochs=100, callbacks=[checkpoint], shuffle=True, batch_size=32) # shuffling data is important
+checkpoint = keras.callbacks.ModelCheckpoint('model.keras', save_best_only=True, save_weights_only=True, mode='max', monitor='val_accuracy')
+model.fit(data, ans, epochs=1000, callbacks=[checkpoint], shuffle=True, batch_size=32, validation_split=0.2) # shuffling data is important
 # fact: early stop is bad bc it'll improve in the long term
