0c1f033049c24f3a81dc114b772c9f7ca96ff379
newwares <newwares123@gmail.com>
2023-09-20 19:49:54 -0400

    use optuna hyperparameter tuner

--- train.py
+++ train.py
@@ -1,22 +1,30 @@
-import pandas as pd
-from preprocess import preprocess
-import lightgbm as lgb
+import os.path
 
-data = preprocess(pd.read_csv('train.csv'))
-ans = data['Survived']
-data = data.drop(['Survived'], axis=1)
-print(data.head())
-data = lgb.Dataset(data, label=ans)
+import pandas as pd
+from lightgbm import early_stopping, log_evaluation
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
+
+from preprocess import preprocess
+import optuna.integration.lightgbm as lgb
+
+csv = preprocess(pd.read_csv('train.csv'))
+ans = csv['Survived']
+csv = csv.drop(['Survived'], axis=1)
+print(csv.head())
+
+x_train, x_test, y_train, y_test = train_test_split(csv, ans, test_size=0.2, random_state=42)
+data = lgb.Dataset(x_train, label=y_train)
+test = lgb.Dataset(x_test, label=y_test)
 params = {
     'objective': 'binary', # binary classification
-    'num_leaves': 15, # num leaves should be less than 2^max_depth,
-    'min_data_in_leaf': 100, # generally 100s to 1000s
-    'metric': 'auc' # "best metric" for classification bc it's immune to unbalanced data
+    'metric': 'auc', # auc is good for binary classification
+    'verbosity': -1
 }
-model = lgb.train(params, data, 10)
-print(min(lgb.cv(params, data, 10, nfold=5)['valid auc-mean'])) # maximize this value
-
+model = lgb.train(params, data, num_boost_round=1000, valid_sets=[data, test], callbacks=[early_stopping(10, first_metric_only=True)])
+print('Accuracy: ', accuracy_score(y_test, model.predict(x_test, num_iteration=model.best_iteration).round()))
+print("Params: \n", model.params)
 test = preprocess(pd.read_csv('test.csv'))
-predictions = model.predict(test)
+predictions = model.predict(test, num_iteration=model.best_iteration)
 output = pd.DataFrame({'PassengerId': test['PassengerId'], 'Survived': predictions.round()}, dtype=int)
 output.to_csv('output.csv', index=False)
